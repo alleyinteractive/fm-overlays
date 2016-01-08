6835bbd2fb9f588cd79852672bd749e9dd924b62
Jake Foster <jake@alleyinteractive.com>
2016-01-08 16:50:33 -0700

    requires and asset version

---
+++ php/class-fm-overlays-admin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * class-fm-overlays-admin.php
+ *
+ * @created     1/8/16 2:00 PM
+ * @author      Alley Interactive
+ * @package     fm-overlays
+ * @description ...
+ *
+ */
+ 
---
+++ static/js/fm-overlays-admin.js
@@ -0,0 +1,10 @@
+/**
+ * ${FILE_NAME}
+ *
+ * @created 1/8/16 2:44 PM
+ * @author Alley Interactive
+ * @package fm-overlays
+ * @description ...
+ *
+ */
+ 
