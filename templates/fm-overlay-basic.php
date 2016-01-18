5464c8a46fb5b775cd413c374bbacc00ae9093ea
Jake Foster <jake@alleyinteractive.com>
2016-01-18 10:15:09 -0700

    initial template for overlay display

--- templates/fm-overlay-basic.php
+++ templates/fm-overlay-basic.php
@@ -5,7 +5,18 @@
  * @created     1/15/16 5:45 PM
  * @author      Alley Interactive
  * @package     fm-overlays
- * @description ...
- *
+ * @description Basic template for displaying fm-overlay
+ * @TODO Finish building content once the fields are finally established.
  */
- 
+if ( empty( $overlay ) ) {
+	return;
+}
+?>
+
+<div class="fm-overlay-wrapper">
+	<div class="fm-overlay-content">
+		<div class="fm-overlay-title">
+			<?php echo esc_html( $overlay->post_title ); ?>
+		</div>
+	</div>
+</div>
