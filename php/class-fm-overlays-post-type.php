18a6bc57b87f1e6a2a5c7a5392b826b38b148fa9
Jake Foster <jake@alleyinteractive.com>
2016-02-01 16:58:59 -0500

    suppress travis

--- php/class-fm-overlays-post-type.php
+++ php/class-fm-overlays-post-type.php
@@ -226,7 +226,7 @@ class Fm_Overlays_Post_Type extends Fm_Overlays_Singleton {
 	 * @param object $query
 	 */
 	public function load_sorted_by_columns( $query ) {
-		if ( ! is_admin() || $this->post_type !== get_query_var( 'post_type' ) ) {
+		if ( ! is_admin() || get_query_var( 'post_type' ) !== $this->post_type ) {
 			return;
 		}
 
