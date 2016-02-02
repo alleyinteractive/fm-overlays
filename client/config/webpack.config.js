9c505a9597731ec006223c407b5f17c676ea9c2a
Jackson Marketon <jmarketon@gmail.com>
2016-02-02 13:23:12 -0600

    Initial files for webpack

---
+++ client/config/webpack.config.js
@@ -0,0 +1,97 @@
+'use strict';
+
+// Webpack dependencies
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var LiveReloadPlugin = require('webpack-livereload-plugin');
+
+// Path definitions
+var buildRoot = path.resolve(__dirname, '../../');
+var appRoot = path.join(buildRoot, 'client/js/app');
+
+// Theme namespace
+var themename = path.join(__dirname, '../../').match(/([^\/]*)\/*$/)[1];
+
+module.exports = {
+  // Two discrete module entry points
+  entry: {
+    global: 'client/js/app/app.js',
+    admin: 'client/js/admin/admin.js'
+  },
+
+  // Define module outputs
+  output: {
+    path: 'static',
+    publicPath: '/wp-content/themes/' + themename + '/static/',
+    filename: 'js/[name].bundle.js'
+  },
+
+  // So we can put config files in config/
+  eslint: {
+    configFile: path.join(__dirname, '.eslintrc')
+  },
+
+  // Where webpack resolves modules
+  resolve: {
+    root: buildRoot,
+    modulesDirectories: [
+      'node_modules'
+    ]
+  },
+
+  // Enable require('jquery') where jquery is already a global
+  externals: {
+    'jquery': 'jQuery'
+  },
+
+  plugins: [
+    new ExtractTextPlugin('css/[name].css'),
+    new LiveReloadPlugin({ appendScriptTag: true }),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader',
+          'css?sourceMap!autoprefixer?{browsers:["last 3 version"]}' +
+          '!sass?outputStyle=compact&sourceMap=true&sourceMapContents=true'
+        )
+      },
+      {
+        test: /\.png(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&minetype=image/svg+xml'
+      },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&minetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&minetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file'
+      },
+    ]
+  }
+}
