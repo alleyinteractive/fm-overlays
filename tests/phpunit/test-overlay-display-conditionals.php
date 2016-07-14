c383022cf30cf8d9a083923884d106e510326316
Griffen Fargo <griffen@alleyinteractive.com>
2016-07-14 12:28:48 -0400

    Updates FM-Overlays Unit Testing
    - Adds base testing class
    - Adds tests for basic overlay creation
    - Adds test for image overlay creation
    - Adds test for rich text storage
    - adds test for overlay content types
    - adds placeholder for overlay conditional display checks

---
+++ tests/phpunit/test-overlay-display-conditionals.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Overlay Conditional Display Unit Testing.
+ *
+ * @package fm-overlays
+ */
+
+class Overlay_Display_Conditionals extends FM_Overlays_UnitTest {
+
+
+	function _parse_conditional( $args ) {
+
+	}
+
+
+	function test_homepage_conditional() {
+
+		// Create overlay
+		// $homepage_overlay_id = $this->factory->post->create( array(
+		// 	'post_type'  => 'fm-overlay',
+		// 	'post_title' => 'hompeage-overlay',
+		// 	'meta_input' => array(
+		// 			'fm_overlays_conditionals' => array(
+		// 				array (
+		// 					'condition_select' =>
+		// 				)
+		// 			)
+		// 		)
+		// 	)
+		// );
+
+
+		$this->assertTrue( true );
+
+
+	}
+}
