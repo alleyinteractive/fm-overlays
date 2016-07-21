26ec78cc6443cdddd53871f1ab0fd56ce10eaecf
Griffen Fargo <griffen@alleyinteractive.com>
2016-07-21 18:41:20 -0400

    refactor checks in `test-conditions.php` to extend base test class
    - Moves all checks in original `tests-conditions.php` into `test-overlay-display-conditionals.php` which extends the base unit test class for overlays

--- tests/phpunit/test-overlay-display-conditionals.php
+++ tests/phpunit/test-overlay-display-conditionals.php
@@ -4,7 +4,8 @@
  *
  * @todo create overlay conditional checks using wp_footer() to verify they are loaded in on the appropriate pages
  *
- * @package fm-overlays
+ * @author 		Alley Interactive
+ * @package 	fm-overlays
  */
 
 class Overlay_Display_Conditionals extends FM_Overlays_UnitTest {
@@ -13,8 +14,11 @@ class Overlay_Display_Conditionals extends FM_Overlays_UnitTest {
 	 * array of conditionals controlling overlay display
 	 */
 	protected $overlay_conditionals = array(
-		array (
+		array(
 			'condition_select' => 'is_home'
+		),
+		array(
+			'condition_select' => 'is_front_page'
 		)
 	);
 
@@ -25,15 +29,51 @@ class Overlay_Display_Conditionals extends FM_Overlays_UnitTest {
 
 	}
 
-	/**
-	 * Check overlay displays on homepage
-	 */
-	function test_homepage_conditional() {
-		// Create overlay
-		$homepage_overlay = $this->create_overlay( true );
+	public function test_homepage() {
+		$this->title = 'is-home';
+		$overlay_id = $this->create_overlay( false );
 
-		$this->assertTrue( true );
+		$this->go_to( '/' );
+		$contents = $this->get_wp_footer();
+		// check for overlay
+		$this->assertContains( '<div class="fm-overlay-wrapper">', $contents );
+		// check for condition
+		$this->assertContains( 'fm-overlay-is_home', $contents );
+	}
 
+	public function test_has_cat() {
+		$this->title = 'has-cat';
+		$this->overlay_conditionals = array();
+		$overlay_id = $this->create_overlay( false );
 
+		$cat_id = $this->factory->category->create( [ 'name' => 'cat' ] );
+		// $overlay_id = $this->factory->post->create( [ 'post_title' => 'has-cat', 'post_status' => 'publish', 'post_date' => '2016-04-01 00:00:00', 'post_type' => 'fm-overlay' ] );
+		$post_id = $this->factory->post->create( [ 'post_title' => 'has-cat-post', 'post_status' => 'publish', 'post_date' => '2016-04-01 00:00:00', 'post_type' => 'post' ] );
+
+		update_post_meta( $overlay_id, 'fm_overlays_conditionals', [ [ 'condition_select' => 'has_category', 'condition_argument_category' => $cat_id ] ] );
+		wp_set_object_terms( $post_id, [ $cat_id ], 'category' );
+
+		$this->go_to( get_permalink( $post_id ) );
+		$contents = $this->get_wp_footer();
+		// check for overlay
+		$this->assertContains( '<div class="fm-overlay-wrapper">', $contents );
+		// check for condition
+		$this->assertContains( 'fm-overlay-has_category', $contents );
+	}
+
+	public function test_is_cat() {
+		$cat_id = $this->factory->category->create( [ 'name' => 'cat' ] );
+		$overlay_id = $this->factory->post->create( [ 'post_title' => 'is-cat', 'post_status' => 'publish', 'post_date' => '2016-04-01 00:00:00', 'post_type' => 'fm-overlay' ] );
+		$post_id = $this->factory->post->create( [ 'post_title' => 'is-cat-post', 'post_status' => 'publish', 'post_date' => '2016-04-01 00:00:00', 'post_type' => 'post' ] );
+
+		update_post_meta( $overlay_id, 'fm_overlays_conditionals', [ [ 'condition_select' => 'is_category', 'condition_argument_category' => $cat_id ] ] );
+		wp_set_object_terms( $post_id, [ $cat_id ], 'category' );
+
+		$this->go_to( get_term_link( $cat_id, 'category' ) );
+		$contents = $this->get_wp_footer();
+		// check for overlay
+		$this->assertContains( '<div class="fm-overlay-wrapper">', $contents );
+		// check for condition
+		$this->assertContains( 'fm-overlay-is_category', $contents );
 	}
 }
